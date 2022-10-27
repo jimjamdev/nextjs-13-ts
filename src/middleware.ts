7e1096f39ce766e1f2558374cf3554f630e0cbd3
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-10-27 21:26:45 +0200

    working with canary

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,7 +22,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('middleware');
+  // Get country
+  //const country = request?.geo.country?.toLowerCase() || 'us';
+  console.log('request', request?.headers?.get('accept-language')?.split(',')[0]?.split('-')[0]);
   // Clone the request headers and set a new header `x-version`
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-version', '13');
