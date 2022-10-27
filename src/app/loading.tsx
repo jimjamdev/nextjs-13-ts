dfeae4a0c5ef803ed5d9faa93d39731e3df0bed6
Ian Jamieson <cogocreative@gmail.com>
2022-10-27 13:41:52 +0200

    cosmos test

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <div className="loading">Loading...</div>
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="loading">Loading...</div>;
 }
