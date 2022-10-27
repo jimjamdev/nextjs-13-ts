95ff8c41a1f94513b9ab037d421fb7670e453ef1
Ian Jamieson <cogocreative@gmail.com>
2022-10-27 15:53:06 +0200

    figure out 404 and current locale using i18n

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   experimental: {
     appDir: true
   },
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    localeDetection: true
+  },
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: {
---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+
+export default function Layout({ locale, children, ...rest }: {
+  children: ReactNode;
+  locale?: string;
+}) {
+  console.log('rest', rest);
+  return <main className="layout">
+    <header><h3 style={{ color: 'tomato' }}>{locale || 'na'}</h3></header>
+    {children}
+  </main>;
+}
