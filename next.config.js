5c13816f582d19add475df58b8cb77c84ba9ac52
Ian Jamieson <cogocreative@gmail.com>
2022-10-27 12:50:38 +0200

    eslint packages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      namespace: 'ui'
+    }
   }
 }
 
