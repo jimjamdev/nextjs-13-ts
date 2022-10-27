fd5d27c6c8a39563629c68eff87ad9efca8328f2
Ian Jamieson <cogocreative@gmail.com>
2022-10-27 17:14:32 +0200

    test middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
+// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('**request**', request);
-  //return request;
-  return NextResponse.redirect(new URL('/about-2', request.url));
-}
+  // Clone the request headers and set a new header `x-version`
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-version', '13');
 
-export const config = {
-  matcher: '/(.*)',
-};
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
+
+  // Set a new response header `x-version`
+  response.headers.set('x-version', '13');
+  return response;
+}
