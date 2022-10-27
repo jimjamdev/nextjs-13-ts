239459d34586922e87fc1119199672c43108a10f
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-10-27 19:25:54 +0200

    middleware loading, build completing. new issue when adding sub-route

--- src/app/faqs/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function Layout({ children }: {
-  children: ReactNode;
-}) {
-  return <section className="faqs">
-    {children}
-  </section>;
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,28 @@
+/*
+import { NextRequest, NextResponse } from 'next/server';
+
+// only run middleware on home page
+export const config = {
+  matcher: '/',
+};
+
+export default function middleware(req: NextRequest) {
+  const country = req.geo?.country?.toLowerCase() || 'us';
+  console.log(`country: ${country}`);
+  const locale = req.headers.get('accept-language')?.split(',')?.[0] || 'en-US';
+
+  // Rewrite the path (`/`) to the localized page (pages/[locale]/[country])
+  req.nextUrl.pathname = `/${locale}/${country}`;
+  return NextResponse.rewrite(req.nextUrl);
+}
+*/
+
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  console.log('middleware');
   // Clone the request headers and set a new header `x-version`
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-version', '13');
