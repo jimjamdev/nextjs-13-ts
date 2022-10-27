2ca2759a09020cd527c79cd3a0eafdca2645b55c
Ian Jamieson <cogocreative@gmail.com>
2022-10-27 15:14:34 +0200

    type fix

--- src/app/[[...locale]]/layout.tsx
+++ src/app/[[...locale]]/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Layout({ params, children }: {
   children: ReactNode;
+  params: { locale?: string };
 }) {
   return <main className="layout">
     <header><h3 style={{ color: 'tomato' }}>{params.locale || 'en'}</h3></header>
