bf32122d3b9bb69d0f89f4174f7107535b5f131b
Ian Jamieson <cogocreative@gmail.com>
2022-10-28 12:46:55 +0200

    missing html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,27 @@
 import { ReactNode } from 'react';
 
-export default function Layout({ locale, children, ...rest }: {
+export default function Layout({
+  locale,
+  children,
+  ...rest
+}: {
   children: ReactNode;
   locale?: string;
 }) {
   console.log('rest', rest);
-  return <main className="layout">
-    <header><h3 style={{ color: 'tomato' }}>{locale || 'nope'}</h3></header>
-    {children}
-  </main>;
+  return (
+    <html>
+      <head>
+        <title>Green Jade</title>
+      </head>
+      <body>
+        <main className="layout">
+          <header>
+            <h3 style={{ color: 'tomato' }}>{locale || 'nope'}</h3>
+          </header>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }
