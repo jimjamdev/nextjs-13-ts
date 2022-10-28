5fb52dd6bac256e0bb02b62a9cfdc79dbb4e574b
Ian Jamieson <cogocreative@gmail.com>
2022-10-28 16:55:14 +0200

    button.stories with args

--- src/ui/atoms/button/button.stories.tsx
+++ src/ui/atoms/button/button.stories.tsx
@@ -27,18 +27,4 @@ button.argTypes = {
   },
 };
 
-/*const Card: Story<{
-  label: string;
-}> = ({ label }) => <p>Label: {label}</p>;
-
-export const CardHello = Card.bind({});
-CardHello.args = {
-  label: 'Hello',
-};
-
-export const CardWorld = Card.bind({});
-CardWorld.args = {
-  label: 'World',
-};*/
-
 button.storyName = 'Button';
