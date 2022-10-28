7d4bc2360b4ba46056f0e3573f08dab825c8b47f
Ian Jamieson <cogocreative@gmail.com>
2022-10-28 16:47:24 +0200

    button.stories test

--- src/ui/atoms/button/button.stories.tsx
+++ src/ui/atoms/button/button.stories.tsx
@@ -1,5 +1,48 @@
+import { action } from '@ladle/react';
+import type { Story } from '@ladle/react';
 import { Button } from './button';
 
-export const button = ({ size }) => <Button size="sm">Hey!</Button>;
+export const button: Story<{
+  label: string;
+  disabled: boolean;
+  count: number;
+  colors: string[];
+  variant: string;
+  size: string;
+}> = ({ count, disabled, label, colors, variant, size }) => (
+  <Button variant={variant} size={size}>{label}</Button>
+);
+
+button.args = {
+  label: 'Hello world',
+  disabled: false,
+  count: 2,
+  colors: ['Red', 'Blue'],
+};
+button.argTypes = {
+  variant: {
+    options: ['primary', 'secondary'],
+    control: { type: 'radio' },
+    defaultValue: 'primary',
+  },
+  size: {
+    options: ['small', 'medium', 'big', 'huuuuge'],
+    control: { type: 'select' },
+  },
+};
+
+/*const Card: Story<{
+  label: string;
+}> = ({ label }) => <p>Label: {label}</p>;
+
+export const CardHello = Card.bind({});
+CardHello.args = {
+  label: 'Hello',
+};
+
+export const CardWorld = Card.bind({});
+CardWorld.args = {
+  label: 'World',
+};*/
 
 button.storyName = 'Button';
