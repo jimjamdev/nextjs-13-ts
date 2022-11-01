8140316c85d81cdd8860bd5ee42873c2c84f8a45
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-11-01 23:36:30 +0100

    middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,24 @@
 import type { NextRequest } from 'next/server';
-// middleware.ts
 import { NextResponse } from 'next/server';
-// import { searchParamsToObject } from '~utils/searchParamsToObject';
+import { searchParamsToObject } from '~utils/searchParamsToObject';
 
 export function middleware(request: NextRequest) {
-  const { headers } = request;
-  const locale = headers?.get('accept-language')?.split(',')?.[0] || 'en-US';
-  const language = locale?.split('-')?.[0] || 'en';
-  /*const country = geo?.country?.toLowerCase() || locale?.split('-')?.[1] || 'us';
-  const params = headers?.get('referer')?.split('?')?.[1];*/
-  return NextResponse.redirect(new URL(`/${language}`, request.url));
+  const { headers, nextUrl, geo } = request;
+  const { pathname, searchParams } = nextUrl;
+  console.log('***Middleware', pathname, searchParams);
+
+  //**
+  // Redirect to correct locale
+  if (pathname === '/') {
+    const locale = headers?.get('accept-language')?.split(',')?.[0] || 'en-US';
+    const language = locale?.split('-')?.[0] || 'en';
+    const country = geo?.country?.toLowerCase() || locale?.split('-')?.[1] || 'us';
+    const params = headers?.get('referer')?.split('?')?.[1];
+    console.log('**locale', locale, language, country, searchParamsToObject(params));
+    return NextResponse.redirect(new URL(`/${language}`, request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
