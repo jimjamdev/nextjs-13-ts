8edc70e1d4e27aa490ca4883041fd437b6297f1d
Ian Jamieson <cogocreative@gmail.com>
2022-11-02 15:31:26 +0100

    middleware.ts

--- src/config/locale.ts
+++ src/config/locale.ts
@@ -1,4 +1,5 @@
 export const localesConfig = {
   defaultLocale: 'en',
   shouldRedirectToBrowserLocale: true,
+  locales: ['en', 'de', 'fr', 'es', 'it', 'pt', 'ru', 'zh', 'ja', 'ko', 'th', 'vi'],
 };
