da2b63072c84848b5c0ab1610b5d2c5578937358
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-11-04 23:13:02 +0100

    remove component

--- cypress/component/ui/button.cy.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "~ui/atoms/button";
-
-describe('Button', () => {
-  it('playground', () => {
-    cy.mount(<Button>Text</Button>);
-    // cy.get('button').should('have.text', 'Text');
-  })
-})
