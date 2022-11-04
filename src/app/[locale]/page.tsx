db8862f9c4f23cb39ae6a07334c9fbf70e5f4817
Ian Jamieson <cogocreative@gmail.com>
2022-11-04 15:44:29 +0100

    cypress config

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from '~ui/atoms/button';
+import { useRef } from "react";
 
 export default function Page({ params, searchParams }: { params: string; searchParams: string }) {
-  return <><h1>Home</h1>{JSON.stringify(params)} {JSON.stringify(searchParams)}<Button>Button</Button></>;
+  const ref = useRef<HTMLDivElement>(null);
+  return <div ref={ref}><h1>Home</h1>{JSON.stringify(params)} {JSON.stringify(searchParams)}<Button>Button</Button></div>;
 }
